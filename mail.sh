52c919909f169aac7458e41032c62ff7a1a31128
Robin Hack <hack.robin@gmail.com>
2018-07-23 19:44:25 +0200

    ipv6 fix

--- mail.sh
+++ mail.sh
@@ -38,9 +38,10 @@ apt-get -y install curl
 passwd
 
 cat << EOF >> /etc/sysctl.conf
-    net.ipv6.conf.all.disable_ipv6 = 1"
+    net.ipv6.conf.all.disable_ipv6 = 1
     net.ipv6.conf.default.disable_ipv6 = 1
     net.ipv6.conf.lo.disable_ipv6 = 1
+    net.ipv6.bindv6only = 1
 EOF
 sysctl -p
 
